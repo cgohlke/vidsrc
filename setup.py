8d517e39c6024b6428fd878b69886a60b9ad4405
Christoph Gohlke <cgohlke@uci.edu>
2019-12-31 20:55:27 -0800

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,4 +1,3 @@
-# -*- coding: utf-8 -*-
 # vidsrc/setup.py
 
 """Vidsrc package setuptools script."""
@@ -7,31 +6,43 @@ import sys
 import re
 
 from setuptools import setup, Extension
+from setuptools.command.build_ext import build_ext as _build_ext
 
-import numpy
 
 with open('vidsrc/vidsrc.cpp') as fh:
     code = fh.read()
 
 version = re.search(r'#define _VERSION_ "(.*?)"', code).groups()[0]
-readme = re.search(r'\*/[\r\n?|\n]{2}/\* (.*)\*/[\r\n?|\n]{2}#', code,
+
+readme = re.search(r'\*/(?:\r\n|\r|\n){2}/\* (.*)\*/(?:\r\n|\r|\n){2}#', code,
                    re.MULTILINE | re.DOTALL).groups()[0]
-license = re.search(r'(Copyright.*)\*/[\r\n?|\n]{2}/\*', code,
-                    re.MULTILINE | re.DOTALL).groups()[0]
 
 description = readme.splitlines()[0][:-1]
-readme = '\n'.join([description, '=' * len(description)]
-                   + readme.splitlines()[1:])
+
+readme = '\n'.join([description, '=' * len(description)] +
+                   readme.splitlines()[1:])
+
+license = re.search(r'(Copyright.*)\*/(?:\r\n|\r|\n){2}/\*', code,
+                    re.MULTILINE | re.DOTALL).groups()[0]
+
 license = license.replace('# ', '').replace('#', '')
 
 if 'sdist' in sys.argv:
     with open('LICENSE', 'w') as fh:
+        fh.write('BSD 3-Clause License\n\n')
         fh.write(license)
     with open('README.rst', 'w') as fh:
         fh.write(readme)
-    numpy_required = '1.11.3'
-else:
-    numpy_required = numpy.__version__
+
+
+class build_ext(_build_ext):
+    """Delay import numpy until build."""
+    def finalize_options(self):
+        _build_ext.finalize_options(self)
+        __builtins__.__NUMPY_SETUP__ = False
+        import numpy
+        self.include_dirs.append(numpy.get_include())
+
 
 DIRECTX_DIR = 'X:/DirectX/include'
 STRMBASE_DIR = 'X:/DirectX/Samples/C++/DirectShow/BaseClasses'
@@ -44,13 +55,16 @@ setup(
     author='Christoph Gohlke',
     author_email='cgohlke@uci.edu',
     url='https://www.lfd.uci.edu/~gohlke/',
+    python_requires='>=3.6',
+    install_requires=['numpy>=1.14.5'],
+    setup_requires=['setuptools>=18.0', 'numpy>=1.14.5'],
+    cmdclass={'build_ext': build_ext},
     packages=['vidsrc'],
     ext_modules=[Extension(
         'vidsrc.vidsrc', ['vidsrc/vidsrc.cpp'],
-        include_dirs=[numpy.get_include(), DIRECTX_DIR, STRMBASE_DIR],
+        include_dirs=[DIRECTX_DIR, STRMBASE_DIR],
         library_dirs=[STRMBASE_DIR + '/libraries'],
         libraries=['STRMBASE', 'Ole32', 'OleAut32', 'strmiids'],)],
-    install_requires=['numpy>=%s' % numpy_required],
     license='BSD',
     zip_safe=False,
     platforms=['Windows'],
@@ -62,11 +76,9 @@ setup(
         'Topic :: Multimedia :: Video',
         'Operating System :: Microsoft :: Windows',
         'Programming Language :: C++',
-        'Programming Language :: Python :: 2',
-        'Programming Language :: Python :: 2.7',
-        'Programming Language :: Python :: 3',
-        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3 :: Only',
         'Programming Language :: Python :: 3.6',
         'Programming Language :: Python :: 3.7',
-        ],
+        'Programming Language :: Python :: 3.8',
+    ],
 )
