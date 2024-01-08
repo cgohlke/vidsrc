29fe7dbeed6e38342ad723bb8041e49638e3a7ae
Christoph Gohlke <cgohlke@cgohlke.com>
2024-01-07 20:49:25 -0800

    Update setup.py

--- setup.py
+++ setup.py
@@ -2,11 +2,11 @@
 
 """Vidsrc package Setuptools script."""
 
-import sys
 import re
+import sys
 
-from setuptools import setup, Extension
-from setuptools.command.build_ext import build_ext as _build_ext
+import numpy
+from setuptools import Extension, setup
 
 
 def search(pattern, code, flags=0):
@@ -17,10 +17,10 @@ def search(pattern, code, flags=0):
     return match.groups()[0]
 
 
-with open('vidsrc/vidsrc.cpp') as fh:
+with open('vidsrc/vidsrc.cpp', encoding='utf-8') as fh:
     code = fh.read()
 
-version = search(r'#define _VERSION_ "(.*?)"', code)
+version = search(r'#define _VERSION_ "(.*?)"', code).replace('.x.x', '.dev0')
 
 readme = search(
     r'(?:\r\n|\r|\n)\"(.*)\"(?:\r\n|\r|\n){2}\#define _VERSION_',
@@ -43,29 +43,15 @@ license = search(
 license = license.replace('# ', '').replace('#', '')
 
 if 'sdist' in sys.argv:
-    with open('LICENSE', 'w') as fh:
+    with open('LICENSE', 'w', encoding='utf-8') as fh:
         fh.write('BSD 3-Clause License\n\n')
         fh.write(license)
-    with open('README.rst', 'w') as fh:
+    with open('README.rst', 'w', encoding='utf-8') as fh:
         fh.write(readme)
 
 
-class build_ext(_build_ext):
-    """Delay import numpy until build."""
-
-    def finalize_options(self):
-        _build_ext.finalize_options(self)
-        if isinstance(__builtins__, dict):
-            __builtins__['__NUMPY_SETUP__'] = False
-        else:
-            setattr(__builtins__, '__NUMPY_SETUP__', False)
-        import numpy
-
-        self.include_dirs.append(numpy.get_include())
-
-
-DIRECTX_DIR = 'X:/DirectX/include'
-STRMBASE_DIR = 'X:/DirectX/Samples/C++/DirectShow/BaseClasses'
+DIRECTX_DIR = 'D:/Programs/DirectX9/include'
+STRMBASE_DIR = 'D:/Programs/DirectX9/Samples/C++/DirectShow/BaseClasses'
 
 setup(
     name='vidsrc',
@@ -73,6 +59,7 @@ setup(
     license='BSD',
     description=description,
     long_description=readme,
+    long_description_content_type='text/x-rst',
     author='Christoph Gohlke',
     author_email='cgohlke@cgohlke.com',
     url='https://www.cgohlke.com',
@@ -81,16 +68,14 @@ setup(
         'Source Code': 'https://github.com/cgohlke/vidsrc',
         # 'Documentation': 'https://',
     },
-    python_requires='>=3.7',
-    install_requires=['numpy>=1.15.1'],
-    setup_requires=['setuptools>=18.0', 'numpy>=1.15.1'],
-    cmdclass={'build_ext': build_ext},
+    python_requires='>=3.9',
+    install_requires=['numpy'],
     packages=['vidsrc'],
     ext_modules=[
         Extension(
             'vidsrc.vidsrc',
             ['vidsrc/vidsrc.cpp'],
-            include_dirs=[DIRECTX_DIR, STRMBASE_DIR],
+            include_dirs=[DIRECTX_DIR, STRMBASE_DIR, numpy.get_include()],
             library_dirs=[STRMBASE_DIR + '/libraries'],
             libraries=['STRMBASE', 'Ole32', 'OleAut32', 'strmiids'],
         )
@@ -106,10 +91,9 @@ setup(
         'Operating System :: Microsoft :: Windows',
         'Programming Language :: C++',
         'Programming Language :: Python :: 3 :: Only',
-        'Programming Language :: Python :: 3.7',
-        'Programming Language :: Python :: 3.8',
         'Programming Language :: Python :: 3.9',
         'Programming Language :: Python :: 3.10',
         'Programming Language :: Python :: 3.11',
+        'Programming Language :: Python :: 3.12',
     ],
 )
