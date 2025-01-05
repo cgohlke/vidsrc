624c976a1c6cc214644d06c6d8eaf0299a5c2c10
Christoph Gohlke <cgohlke@cgohlke.com>
2025-01-05 13:17:34 -0800

    Update tests/test_vidsrc.py

--- tests/test_vidsrc.py
+++ tests/test_vidsrc.py
@@ -1,26 +1,44 @@
 # test_vidsrc.py
 
+import numpy
+import pytest
 import vidsrc
 
-__doc__ = vidsrc.__doc__
 
-print('module:', vidsrc)
-print('module version:', vidsrc.__version__)
-print('module doc:', vidsrc.__doc__)
-print()
+def test_module() -> None:
+    """Test vidsrc module."""
 
-video = vidsrc.VideoSource('test.avi')
+    assert 'version' in vidsrc.__doc__
+    assert vidsrc.__version__
 
-print('filename:', video.filename)
-print('frames:', len(video))
-print('framerate:', video.framerate)
-print('duration:', video.duration)
-print('shape:', video.shape)
-print('frame averages:')
-for i, frame in enumerate(video):
-    print(f'{i:3} {frame.mean():.2f}')
 
-if __name__ == '__main__':
-    import doctest
+def test_videosource() -> None:
+    """Test VideoSource class."""
+    video = vidsrc.VideoSource('test.avi')
+    assert video.filename == 'test.avi'
+    assert len(video) == 48
+    assert pytest.approx(video.framerate, abs=1e-2) == 29.97
+    assert pytest.approx(video.duration, abs=1e-2) == 1.6016
+    assert video.shape == (48, 64, 64, 3)
+    assert video[0].dtype == numpy.uint8
+    assert video[0].shape == (64, 64, 3)
+    mean = numpy.asarray([frame.mean() for frame in video])
+    numpy.testing.assert_allclose(
+        mean[[1, -1]], [6.871582, 18.197266], atol=1e-3
+    )
 
-    doctest.testmod()
+
+def test_videosource_options() -> None:
+    """Test VideoSource class with options."""
+    video = vidsrc.VideoSource('test.avi', framerate=30.0, grayscale=True)
+    assert video.filename == 'test.avi'
+    assert len(video) == 48
+    assert pytest.approx(video.framerate, abs=1e-2) == 30.0
+    assert pytest.approx(video.duration, abs=1e-2) == 1.6016
+    assert video.shape == (48, 64, 64)
+    assert video[0].dtype == numpy.float64
+    assert video[0].shape == (64, 64)
+    mean = numpy.asarray([frame.mean() for frame in video])
+    numpy.testing.assert_allclose(
+        mean[[1, -1]], [0.026947, 0.071362], atol=1e-3
+    )
