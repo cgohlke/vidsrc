ee4c8f1330e19d1928fd6e6b5a84022c8145bf6e
Christoph Gohlke <cgohlke@cgohlke.com>
2025-02-22 10:28:38 -0800

    Update vidsrc/__init__.py

--- vidsrc/__init__.py
+++ vidsrc/__init__.py
@@ -1,6 +1,7 @@
-# -*- coding: utf-8 -*-
 # vidsrc/__init__.py
 
 __all__ = ['__version__', 'VideoSource']
 
 from .vidsrc import VideoSource, __doc__, __version__
+
+VideoSource.__module__ == 'vidsrc'
